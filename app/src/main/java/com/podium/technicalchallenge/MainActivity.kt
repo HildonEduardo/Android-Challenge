a785aed5e5a7a9a3a21cc5b85042a5cc2bc4c8ba
hildonlima <hildon.eduardo@gmail.com>
2024-05-01 12:20:55 -0400

    Organize build.gradle and completely setup jetpack compose.

--- app/src/main/java/com/podium/technicalchallenge/MainActivity.kt
+++ app/src/main/java/com/podium/technicalchallenge/MainActivity.kt
@@ -1,18 +1,47 @@
 package com.podium.technicalchallenge
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.podium.technicalchallenge.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.podium.technicalchallenge.ui.theme.PodiumTechinalChallengeTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PodiumTechinalChallengeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PodiumTechinalChallengeTheme {
+        Greeting("Android")
     }
 }
